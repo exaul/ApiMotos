760fe2076fc62874e5b59a5d240b76c078d6324b
exaul <137360873+exaul@users.noreply.github.com>
2024-01-15 19:47:46 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/MotosController.cs
@@ -0,0 +1,45 @@
+﻿using APIMotos.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIMotos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MotosController : ControllerBase
+    {
+        private static List<Motos> _motos = new List<Motos>
+        {
+            new Motos
+            {
+                Id = 1,
+                Marca = "Honda",
+                Modelo = "CB1 Star 2023"
+
+            },
+            new Motos
+            {
+                Id = 2,
+                Marca = "BMW",
+                Modelo = "F 900R"
+
+            },
+            new Motos
+            {
+                Id = 3,
+                Marca = "Yamaha",
+                Modelo = "YBR-125"
+
+            },
+        };
+
+        [HttpGet]
+
+        public IEnumerable<Motos> GetAllMotos() 
+        { 
+            return _motos;
+        
+        }
+
+    }
+}
---
+++ Models/Motos.cs
@@ -0,0 +1,15 @@
+﻿namespace APIMotos.Models
+{
+    public class Motos
+    {
+        //prop
+        
+        public int Id { get; set; }
+
+        public string Marca { get; set; }
+
+        public string Modelo { get; set; }
+
+
+    }
+}
